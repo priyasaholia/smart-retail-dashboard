925e0ffd32a001a869deb8ea506092fbc7cdc5a4
priyasaholia <2023002356.gcet@cvmu.edu.in>
2026-01-15 23:01:11 +0530

    Complete alert ingestion API and dashboard integration

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import home, dashboard
+from .views import create_alert_api
+
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('dashboard/', dashboard, name='dashboard'),
+    path("", home, name="home"),
+    path("dashboard/", dashboard, name="dashboard"),
+    path("api/alerts/", create_alert_api),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Product, Alert
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
 
 def home(request):
     return render(request, 'home.html')
@@ -14,3 +18,41 @@ def dashboard(request):
 
     return render(request, 'dashboard.html', context)
 
+@csrf_exempt
+def create_alert_api(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+
+            message = data.get("message")
+            source = data.get("source", "external")
+
+            if not message:
+                return JsonResponse(
+                    {"error": "Message is required"},
+                    status=400
+                )
+
+            alert = Alert.objects.create(
+                message=message,
+                source=source
+            )
+
+            return JsonResponse(
+                {
+                    "status": "success",
+                    "alert_id": alert.id
+                },
+                status=201
+            )
+
+        except json.JSONDecodeError:
+            return JsonResponse(
+                {"error": "Invalid JSON"},
+                status=400
+            )
+
+    return JsonResponse(
+        {"error": "Only POST method allowed"},
+        status=405
+    )
